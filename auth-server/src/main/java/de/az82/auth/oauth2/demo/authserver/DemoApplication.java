ab79bb84f912be7e0bdd6a1ab4f74acdf6a0710b
Andreas Zitzelsberger <az@az82.de>
2022-01-11 14:07:33 +0100

    Initial import of auth-server and resource-server

---
+++ auth-server/src/main/java/de/az82/auth/oauth2/demo/authserver/DemoApplication.java
@@ -0,0 +1,13 @@
+package de.az82.auth.oauth2.demo.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ resource-server/src/main/java/de/az82/auth/oauth2/demo/resourceserver/DemoApplication.java
@@ -0,0 +1,13 @@
+package de.az82.auth.oauth2.demo.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
